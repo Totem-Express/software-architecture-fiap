5c624844c9fa4d7061f282716c472d0ebd4116dc
carlosmuller <kdu428@gmail.com>
2024-12-27 14:14:22 -0300

    test: TotemExpressApplication

--- src/test/java/br/com/fiap/totem_express/TestTotemExpressApplication.java
+++ src/test/java/br/com/fiap/totem_express/TestTotemExpressApplication.java
@@ -1,6 +1,11 @@
 package br.com.fiap.totem_express;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import static org.mockito.Mockito.*;
 
 public class TestTotemExpressApplication {
 
@@ -8,4 +13,17 @@ public class TestTotemExpressApplication {
 		SpringApplication.from(TotemExpressApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
+	@Test
+	void should_add_cors_correctly() {
+		TotemExpressApplication totemExpressApplication = new TotemExpressApplication();
+		CorsRegistry corsRegistry = mock(CorsRegistry.class);
+		CorsRegistration corsRegistration = mock(CorsRegistration.class);
+		when(corsRegistry.addMapping("/**")).thenReturn(corsRegistration);
+		when(corsRegistration.allowedMethods("*")).thenReturn(corsRegistration);
+		when(corsRegistration.allowedOrigins("*")).thenReturn(corsRegistration);
+		totemExpressApplication.addCorsMappings(corsRegistry);
+		verify(corsRegistry).addMapping("/**");
+		verify(corsRegistration).allowedMethods("*");
+		verify(corsRegistration).allowedOrigins("*");
+	}
 }
