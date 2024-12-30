68821fb04e1c200766dd84367e19ac1d3b3e2fb0
Ana de Jesus <anadejesus.dev@gmail.com>
2024-12-30 19:58:22 -0300

    test(product controller): controller transacoes de produtos

--- src/test/java/br/com/fiap/totem_express/presentation/product/ProductControllerTest.java
+++ src/test/java/br/com/fiap/totem_express/presentation/product/ProductControllerTest.java
@@ -5,10 +5,13 @@ import br.com.fiap.totem_express.application.product.DeleteProductUseCase;
 import br.com.fiap.totem_express.application.product.FindProductsByCategoryUseCase;
 import br.com.fiap.totem_express.application.product.UpdateProductUseCase;
 import br.com.fiap.totem_express.application.product.output.ProductView;
+import br.com.fiap.totem_express.domain.product.Category;
 import br.com.fiap.totem_express.infrastructure.jwt.JWTService;
 import br.com.fiap.totem_express.presentation.product.request.CreateProductRequest;
+import br.com.fiap.totem_express.presentation.product.request.UpdateProductRequest;
+import br.com.fiap.totem_express.shared.invariant.InvariantException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,17 +22,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
-import static br.com.fiap.totem_express.domain.product.Category.DISH;
+import static br.com.fiap.totem_express.domain.product.Category.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -44,7 +47,10 @@ class ProductControllerTest {
     private JWTService jwtService;
 
     @Autowired
-    private JacksonTester<CreateProductRequest> request;
+    private JacksonTester<CreateProductRequest> createRequest;
+
+    @Autowired
+    private JacksonTester<UpdateProductRequest> updateRequest;
 
     @Autowired
     private JacksonTester<ProductView.SimpleView> view;
@@ -58,6 +64,11 @@ class ProductControllerTest {
     @MockBean
     private FindProductsByCategoryUseCase findAllByCategoryUseCase;
 
+    @BeforeEach
+    void setUp() {
+        jwtService = mock(JWTService.class);
+    }
+
     @Test
     void should_return_http_200_when_the_request_is_valid() throws Exception {
         var createProduct = new CreateProductRequest(
@@ -69,7 +80,7 @@ class ProductControllerTest {
         var response = mockMvc
                 .perform(post("/api/product")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(request
+                        .content(createRequest
                                 .write(createProduct)
                                 .getJson()
                         )
@@ -114,15 +125,112 @@ class ProductControllerTest {
         verify(deleteUseCase).delete(productId);
     }
 
-//    TODO -> pensar em como testar a falha (precisa fazer a verificacao se o id existe no controller)
-//    @Test
-//    void should_return_http_404_when_product_not_found() throws Exception {
-//        Long productId = 1L;
-//
-//        doThrow(new IllegalArgumentException("Product not found")).when(deleteUseCase).delete(productId);
-//
-//        mockMvc.perform(delete("/api/product/{id}", productId))
-//                .andExpect(status().isNotFound())
-//                .andExpect(content().string("Product not found"));
-//    }
+    @Test
+    void should_return_http_404_when_product_to_delete_not_found() throws Exception {
+        Long productId = 1L;
+
+        doThrow(new IllegalArgumentException("Product must exists invalid id " + productId)).when(deleteUseCase).delete(productId);
+
+        mockMvc.perform(delete("/api/product/{id}", productId))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void should_return_http_200_when_update_is_successful() throws Exception {
+        Long productId = 29L;
+
+        var updateProduct = new UpdateProductRequest(
+                productId, "Coca-Cola 500ml",
+                "Refrescante e geladinha. Uma bebida assim refresca a vida.",
+                "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kNXBvqQj/200/200/original?country=br",
+                new BigDecimal("12.90"), DRINK);
+
+        var expectedView = new ProductView.SimpleView(
+                productId, "Coca-Cola 500ml",
+                "Refrescante e geladinha. Uma bebida assim refresca a vida.",
+                "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kNXBvqQj/200/200/original?country=br",
+                new BigDecimal("12.90"), DRINK);
+
+        when(updateUseCase.update(any())).thenReturn(Optional.of(expectedView));
+
+        var response = mockMvc
+                .perform(put("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateRequest
+                                .write(updateProduct)
+                                .getJson()
+                        )
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(OK.value());
+        assertThat(response.getContentAsString()).isEqualTo(view.write(expectedView).getJson());
+    }
+
+    @Test
+    void should_return_http_404_when_product_not_found() throws Exception {
+        var updateProduct = new UpdateProductRequest(
+                999L,
+                "Del Valle 700ml",
+                "Néctar de fruta nos sabores uva ou laranja.",
+                "https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kNXWVFLM/200/200/original?country=br",
+                new BigDecimal("11.90"), DRINK);
+
+        when(updateUseCase.update(any())).thenReturn(Optional.empty());
+
+        mockMvc.perform(put("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateRequest
+                                .write(updateProduct)
+                                .getJson()
+                        )
+                )
+                .andExpect(status().isNotFound());
+
+        verify(updateUseCase).update(updateProduct);
+    }
+
+    @Test
+    void should_return_http_200_with_products_when_category_is_valid_and_products_exist() throws Exception {
+        String categoryName = "DISH";
+
+        var cheddarMcMeltProduct = new ProductView.SimpleView(1L, "Cheddar McMelt",
+                "Um hamburguer (100% carne bovina), molho lacteo com queijo tipo cheddar, cebola ao molho shoyu e pao escuro com gergelim.",
+                "https://example.com/product1.jpg",
+                new BigDecimal("19.90"), DISH);
+
+        var bigMacProduct = new ProductView.SimpleView(2L, "Big Mac",
+                "Dois hambúrgueres, alface, queijo, molho especial, cebola, picles e pão com gergelim.",
+                "https://example.com/product2.jpg",
+                new BigDecimal("21.90"), DISH);
+
+        when(findAllByCategoryUseCase.findAllByCategory(DISH)).thenReturn(List.of(cheddarMcMeltProduct, bigMacProduct));
+
+        mockMvc.perform(get("/api/product/{categoryName}", categoryName))
+                .andExpect(status().isOk());
+
+        verify(findAllByCategoryUseCase).findAllByCategory(DISH);
+    }
+
+    @Test
+    void should_return_http_204_when_category_is_valid_but_no_products_exist() throws Exception {
+        String categoryName = "DESSERT";
+
+        when(findAllByCategoryUseCase.findAllByCategory(DESSERT)).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/product/{categoryName}", categoryName))
+                .andExpect(status().isNoContent());
+
+        verify(findAllByCategoryUseCase).findAllByCategory(DESSERT);
+    }
+
+    @Test
+    void should_return_http_404_when_category_is_invalid() throws Exception {
+        String invalidCategoryName = "INVALID_CATEGORY";
+
+        mockMvc.perform(get("/api/product/{categoryName}", invalidCategoryName))
+                .andExpect(status().isNotFound());
+    }
+
 }
