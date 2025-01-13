f6f3abbf9392a4e03a49406d8d1e49a27d6c403b
carlosmuller <kdu428@gmail.com>
2025-01-13 20:23:22 -0300

    test: DeleteProductUseCaseImpl

--- src/main/java/br/com/fiap/totem_express/application/product/impl/DeleteProductUseCaseImpl.java
+++ src/main/java/br/com/fiap/totem_express/application/product/impl/DeleteProductUseCaseImpl.java
@@ -2,13 +2,9 @@ package br.com.fiap.totem_express.application.product.impl;
 
 import br.com.fiap.totem_express.application.product.DeleteProductUseCase;
 import br.com.fiap.totem_express.application.product.ProductGateway;
-import br.com.fiap.totem_express.domain.payment.Payment;
 import br.com.fiap.totem_express.domain.product.Product;
-import br.com.fiap.totem_express.shared.invariant.InvariantException;
 
-import java.util.Optional;
 
-//TODO: teste
 public class DeleteProductUseCaseImpl implements DeleteProductUseCase {
 
     private final ProductGateway gateway;
